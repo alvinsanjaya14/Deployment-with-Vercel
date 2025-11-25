0ed176d9ff185405803b08aef590e894f6db4d24
Alvin Sanjaya <alvinsanjaya182@gmail.com>
2025-11-25 23:18:54 +0700

    update prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,29 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
+import path from 'path';
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
+declare global {
+  // eslint-disable-next-line no-var
+  var cachedPrisma: PrismaClient;
+}
 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+// Workaround to find the db file in production
+const filePath = path.join(process.cwd(), 'prisma/dev.db');
+const config = {
+  datasources: {
+    db: {
+      url: 'file:' + filePath,
+    },
+  },
+};
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+let prisma: PrismaClient;
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient(config);
+} else {
+  if (!global.cachedPrisma) {
+    global.cachedPrisma = new PrismaClient(config);
+  }
+  prisma = global.cachedPrisma;
+}
+
+export const db = prisma;
